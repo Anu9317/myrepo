104ebe88052db95841819d787ec1f59423db4a54
Anu <Anu@DESKTOP-952Q9QQ>
2025-05-06 19:45:06 -0400

    Commiting first project

---
+++ SampleMaven/src/main/java/stringexample/Example2.java
@@ -0,0 +1,26 @@
+package stringexample;
+
+public class Example2 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	StringBuffer a = new StringBuffer("hello");
+	System.out.println(a);
+	StringBuilder b = new StringBuilder("hi");
+	System.out.println(b);
+	//insert
+	System.out.println(a.insert(3, "world"));
+	//Append
+	System.out.println(a.append("hi"));
+	//replace
+	System.out.println(a.replace(3, 5, "world"));
+	//delete
+	System.out.println(a.delete(2, 4));
+
+		}
+	}
+
+
+	
+
+
